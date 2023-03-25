30c4eb5616cfe16b0207ee55d497148ca15340a8
jacckmark@gmail.com <jacckmark@gmail.com>
2023-03-25 17:00:28 +0100

    added first info for chapter 10

---
+++ 10-merge_sort/example.js
@@ -0,0 +1,10 @@
+function mergeSort(array) {
+  return array;
+}
+
+const unsortedArr1 = [1, 4, 55, 890, 2, 3, 9, 0];
+console.log("\n*MERGE SORT");
+console.log("array before merge sort");
+console.log(unsortedArr1);
+console.log("array after merge sort");
+console.log(mergeSort(unsortedArr1));
---
+++ 10-merge_sort/notes.md
@@ -0,0 +1,3 @@
+# MERGE SORT
+
+1. Merge sort is a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.
